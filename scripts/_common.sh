651f6ad9e9864bdaeb9bb020665728b6e31de45f
ericgaspar <junk.eg@free.fr>
2022-01-10 19:30:22 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="sqlite3"
+pkg_dependencies=""
 
 #=================================================
 # PERSONAL HELPERS
