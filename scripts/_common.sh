28d5a9691b30eed304e308d390467d5bbacfb9bb
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-03 18:33:29 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="sqlite3"
+#REMOVEME? pkg_dependencies="sqlite3"
 
 #=================================================
 # PERSONAL HELPERS
